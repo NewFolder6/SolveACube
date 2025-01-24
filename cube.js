dafd115c44b137055dda6b947bd31bb82a52a7f3
New Folder (6) <150495167+NewFolder6@users.noreply.github.com>
2025-01-24 12:50:33 -0800

    feat(cube): refactor cube logic into class structure and update main entry point

---
+++ cube.js
@@ -0,0 +1,156 @@
+export default class Cube {
+    static Direction = {
+        UP: 'UP',
+        DOWN: 'DOWN',
+        LEFT: 'LEFT',
+        RIGHT: 'RIGHT',
+        FORWARD: 'FRONT',
+        BACKWARD: 'BACKWARD'
+    };
+
+    static PieceType = {
+        EDGE: 'EDGE',
+        CORNER: 'CORNER',
+        CENTER: 'CENTER'
+    };
+
+    static PieceColor = {
+        WHITE: 'WHITE',
+        YELLOW: 'YELLOW',
+        ORANGE: 'ORANGE',
+        RED: 'RED',
+        GREEN: 'GREEN',
+        BLUE: 'BLUE'
+    };
+
+    ScrambleCube(steps){
+        for (let i = 0; i < steps; i++){
+            let clockwise = Math.random() >= 0.5;
+            switch (Math.floor(Math.random() * 6)){
+                case 0:
+                    RotateLayer(Direction.UP, clockwise);
+                    break;
+                case 1:
+                    RotateLayer(Direction.DOWN, clockwise);
+                    break;
+                case 2:
+                    RotateLayer(Direction.LEFT, clockwise);
+                    break;
+                case 3:
+                    RotateLayer(Direction.RIGHT, clockwise);
+                    break;
+                case 4:
+                    RotateLayer(Direction.FORWARD, clockwise);
+                    break;
+                case 5:
+                    RotateLayer(Direction.BACKWARD, clockwise);
+                    break;
+            }
+        }
+    }
+
+    RotateLayer(cube, direction, layer, clockwise){
+        // TODO
+        GetLayerPieces(cube, direction, layer).forEach(piece => {
+            switch (direction){
+                case Direction.UP:
+                    // TODO
+                    cube.pieces[piece.Index].X = clockwise ? piece.Z : -piece.Z;
+                    cube.pieces[piece.Index].Z = clockwise ? -piece.X : piece.X;
+                    break;
+                case Direction.DOWN:
+                    cube.pieces[piece.Index].X = clockwise ? -piece.Z : piece.Z;
+                    cube.pieces[piece.Index].Z = clockwise ? piece.X : -piece.X;
+                    break;
+                case Direction.LEFT:
+                    cube.pieces[piece.Index].Y = clockwise ? piece.Z : -piece.Z;
+                    cube.pieces[piece.Index].Z = clockwise ? -piece.Y : piece.Y;
+                    break;
+                case Direction.RIGHT:
+                    cube.pieces[piece.Index].Y = clockwise ? -piece.Z : piece.Z;
+                    cube.pieces[piece.Index].Z = clockwise ? piece.Y : -piece.Y;
+                    break;
+                case Direction.FRONT:
+                    cube.pieces[piece.Index].X = clockwise ? piece.Y : -piece.Y;
+                    cube.pieces[piece.Index].Y = clockwise ? -piece.X : piece.X;
+                    break;
+                case Direction.BACKWARD:
+                    cube.pieces[piece.Index].X = clockwise ? -piece.Y : piece.Y;
+                    cube.pieces[piece.Index].Y = clockwise ? piece.X : -piece.X;
+                    break;
+            }
+        });
+    }
+
+    InitializePiece(index, cubeSize, x, y, z){
+        var faces = 0;
+
+        // green side is front 
+        const front = (z === 0 ? Cube.PieceColorGREEN : false);
+        const back = (z === cubeSize - 1 ? Cube.PieceColorBLUE : false);
+        const left = (x === 0 ? Cube.PieceColorORANGE : false);
+        const right = (x === cubeSize - 1 ? Cube.PieceColorRED : false);
+        const top = (y === cubeSize - 1 ? Cube.PieceColorWHITE : false);
+        const bottom = (y === 0 ? Cube.PieceColorYELLOW : false);
+        
+        [front, back, left, right, top, bottom].forEach(side => {
+            if (side) faces++;
+        });
+
+        if (faces === 0) return false;
+
+        var type = (faces === 1 ? PieceType.CENTER : (faces === 2 ? PieceType.EDGE : PieceType.CORNER));
+        const offset = Math.floor(cubeSize / 2);
+        return {
+            Index: index,
+            X: x - offset,
+            Y: y - offset,
+            Z: z - offset,
+            Type: type,
+            Faces: faces,
+            Color: {
+                Front: front,
+                Back: back,
+                Left: left,
+                Right: right,
+                Top: top,
+                Bottom: bottom
+            }
+        };
+    }
+
+    GetLayerPieces(cube, direction, layer){
+        var offset = Math.floor(cube.size / 2);
+        layer--;
+        switch (direction){
+            case Direction.UP:
+                return cube.pieces.filter(piece => piece.Y === offset - layer);
+            case Direction.DOWN:
+                return cube.pieces.filter(piece => piece.Y === -offset + layer);
+            case Direction.LEFT:
+                return cube.pieces.filter(piece => piece.X === -offset + layer);
+            case Direction.RIGHT:
+                return cube.pieces.filter(piece => piece.X === offset - layer);
+            case Direction.FRONT:
+                return cube.pieces.filter(piece => piece.Z === -offset + layer);
+            case Direction.BACKWARD:
+                return cube.pieces.filter(piece => piece.Z === offset - layer);
+        }
+    }
+
+    constructor(cubeSize){
+            this.size = cubeSize;
+            this.pieces = [];
+            var i = 0;
+            for (let x = 0; x < cubeSize; x++){
+                for (let y = 0; y < cubeSize; y++){
+                    for (let z = 0; z < cubeSize; z++){
+                        let piece = this.InitializePiece(i, cubeSize, x, y, z);
+                        if (!piece) continue;
+                        this.pieces.push(piece);
+                        i++;
+                    }
+                }
+            }
+        }
+}
---
+++ main.js
@@ -0,0 +1,7 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import Cube from './cube.js';
+
+var cube = new Cube(3);
+
+console.log(cube.pieces);
