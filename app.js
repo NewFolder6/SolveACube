25604bfb7506da35463815c83594d3ccc109ab31
New Folder (6) <150495167+NewFolder6@users.noreply.github.com>
2025-01-23 19:49:19 -0800

    Refactor cube initialization and add scrambling functionality

--- app.js
+++ app.js
@@ -1,54 +1,111 @@
-import * as THREE from 'three';
+//import * as THREE from 'three';
+
 const Direction = {
     UP: 'UP',
     DOWN: 'DOWN',
     LEFT: 'LEFT',
     RIGHT: 'RIGHT',
-    FORWARD: 'FORWARD',
+    FORWARD: 'FRONT',
     BACKWARD: 'BACKWARD'
 };
 
-const Types = {
+const PieceType = {
     EDGE: 'EDGE',
     CORNER: 'CORNER',
     CENTER: 'CENTER'
 }
 
+const PieceColor = {
+    WHITE: 'WHITE',
+    YELLOW: 'YELLOW',
+    ORANGE: 'ORANGE',
+    RED: 'RED',
+    GREEN: 'GREEN',
+    BLUE: 'BLUE'
+}
+
 // cannot be 1
-const cubeSize = 3;
+const cubeSize = 2;
+const offset = Math.floor(cubeSize / 2);
 var pieces = [];
 
-function InitalizePiece(index, x, y, z){
+function ScrambleCube(steps){
+    for (let i = 0; i < steps; i++){
+        const clockwise = Math.random() >= 0.5;
+    switch (Math.floor(Math.random() * 6)){
+        case 0:
+            RotateLayer(Direction.UP, clockwise);
+            break;
+        case 1:
+            RotateLayer(Direction.DOWN, clockwise);
+            break;
+        case 2:
+            RotateLayer(Direction.LEFT, clockwise);
+            break;
+        case 3:
+            RotateLayer(Direction.RIGHT, clockwise);
+            break;
+        case 4:
+            RotateLayer(Direction.FORWARD, clockwise);
+            break;
+        case 5:
+            RotateLayer(Direction.BACKWARD, clockwise);
+            break;
+    }
+    }
+}
+
+function InitializePiece(index, x, y, z){
+    var faces = 0;
+
+    // green side is front 
+    const front = (z === 0 ? PieceColor.GREEN : false);
+    const back = (z === cubeSize - 1 ? PieceColor.BLUE : false);
+    const left = (x === 0 ? PieceColor.ORANGE : false);
+    const right = (x === cubeSize - 1 ? PieceColor.RED : false);
+    const top = (y === cubeSize - 1 ? PieceColor.WHITE : false);
+    const bottom = (y === 0 ? PieceColor.YELLOW : false);
+    
+    [front, back, left, right, top, bottom].forEach(side => {
+        if (side) faces++;
+    });
+    
+    if (faces === 0) return false;
 
     // TODO
-    var orientation = Direction.UP;
-    var type;
-    if (x == 0 || x == cubeSize - 1){
-        
-    }
+    var type = (faces === 1 ? PieceType.CENTER : (faces === 2 ? PieceType.EDGE : PieceType.CORNER));
 
     pieces.push(index, {
-        X: x,
-        Y: y,
-        Z: z,
-        Orentiation: orientation,
-        Type: type
+        X: x - offset,
+        Y: y - offset,
+        Z: z - offset,
+        Type: type,
+        Color: {
+            Front: front,
+            Back: back,
+            Left: left,
+            Right: right,
+            Top: top,
+            Bottom: bottom
+        }
     });
 
     pieces[index].Direction = Direction.BACKWARD;
 }
 
-function InitilizeCube(){
-    let i = 0;
+function InitializeCube(){
+    var i = 0;
     for (let x = 0; x < cubeSize; x++){
         for (let y = 0; y < cubeSize; y++){
             for (let z = 0; z < cubeSize; z++){
-                if (x == 0 || x == cubeSize - 1 || y == 0 || y == cubeSize - 1 || z == 0 || z == cubeSize - 1){
-                    InitalizePiece(i, x, y, z);
+                if(InitalizePiece(i, x, y, z) != false){
                     i++;
                 }
             }
         }
     }
+    console.log(i);
 }
 
+InitilizeCube();
+console.log(pieces);
